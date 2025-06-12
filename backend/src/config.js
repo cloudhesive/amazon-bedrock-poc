353b920900f410ca929890ea6a15ed10a93582ab
Santiago <santiago.labatut@cloudhesive.com>
2025-06-12 12:20:19 -0300

    Add cloudfront integration to host website

--- backend/src/config.js
+++ backend/src/config.js
@@ -1,9 +1,5 @@
 const config = {
-    region: process.env.REGION || 'us-east-1',
-    modelId: process.env.MODEL_ID || 'anthropic.claude-v2',
-    maxTokens: parseInt(process.env.MAX_TOKENS, 10) || 300, // Limita la longitud de la respuesta.
-    temperature: parseFloat(process.env.TEMPERATURE) || 0.1, // Controla la aleatoriedad de las respuestas. 
-    topP: parseFloat(process.env.TOP_P) || 0.9, // Restringe las opciones de respuesta basadas en probabilidades acumulativas.
+    region: process.env.REGION,
     userPoolId: process.env.USER_POOL_ID,
     clientId: process.env.USER_POOL_CLIENT_ID,
     chatHistoryTable: process.env.CHAT_HISTORY_TABLE,
